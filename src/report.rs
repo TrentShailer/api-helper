337e47a50451e4337e535490566ac7ed7cedd561
Trent Shailer <trent.shailer@gmail.com>
2025-06-22 17:42:15 +1200

    feat: add context back to internal server errors

--- src/report.rs
+++ src/report.rs
@@ -10,6 +10,9 @@ pub fn report_error<E: Display>(error: E) {
 pub trait InternalServerError<T> {
     #[track_caller]
     fn internal_server_error(self) -> Result<T, ErrorResponse>;
+
+    #[track_caller]
+    fn internal_server_error_context<C: Display>(self, context: C) -> Result<T, ErrorResponse>;
 }
 
 impl<T, E: Display> InternalServerError<T> for Result<T, E> {
@@ -20,6 +23,14 @@ impl<T, E: Display> InternalServerError<T> for Result<T, E> {
             ErrorResponse::internal_server_error()
         })
     }
+
+    #[track_caller]
+    fn internal_server_error_context<C: Display>(self, context: C) -> Result<T, ErrorResponse> {
+        self.map_err(|error| {
+            report_error(format!("{context}: {error}"));
+            ErrorResponse::internal_server_error()
+        })
+    }
 }
 
 impl<T> InternalServerError<T> for Option<T> {
@@ -27,6 +38,14 @@ impl<T> InternalServerError<T> for Option<T> {
     fn internal_server_error(self) -> Result<T, ErrorResponse> {
         self.ok_or_else(|| {
             report_error("Option was None");
+            ErrorResponse::internal_server_error()
+        })
+    }
+
+    #[track_caller]
+    fn internal_server_error_context<C: Display>(self, context: C) -> Result<T, ErrorResponse> {
+        self.ok_or_else(|| {
+            report_error(format!("Option (`{context}`) was None"));
             ErrorResponse::internal_server_error()
         })
     }
