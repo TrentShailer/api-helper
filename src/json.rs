70ee7036607a551c1be13441b3a32ab415afb291
Trent Shailer <trent.shailer@gmail.com>
2025-06-22 14:09:41 +1200

    feat: make JSON extractor optional

--- src/json.rs
+++ src/json.rs
@@ -1,15 +1,46 @@
-use axum::{extract::FromRequest, response::IntoResponse};
-use serde::Serialize;
+use axum::{
+    extract::{FromRequest, OptionalFromRequest, Request},
+    response::IntoResponse,
+};
+use serde::{Serialize, de::DeserializeOwned};
 
 use crate::ErrorResponse;
 
-#[derive(FromRequest)]
-#[from_request(via(axum::Json), rejection(ErrorResponse))]
 pub struct Json<T>(pub T);
 
 impl<T: Serialize> IntoResponse for Json<T> {
     fn into_response(self) -> axum::response::Response {
         let Self(value) = self;
         axum::Json(value).into_response()
+    }
+}
+
+impl<T, S> FromRequest<S> for Json<T>
+where
+    T: DeserializeOwned,
+    S: Send + Sync,
+{
+    type Rejection = ErrorResponse;
+
+    async fn from_request(req: Request, state: &S) -> Result<Self, Self::Rejection> {
+        <axum::Json<_> as axum::extract::FromRequest<S>>::from_request(req, state)
+            .await
+            .map_err(ErrorResponse::from)
+            .map(|value| Json(value.0))
+    }
+}
+
+impl<T, S> OptionalFromRequest<S> for Json<T>
+where
+    T: DeserializeOwned,
+    S: Send + Sync,
+{
+    type Rejection = ErrorResponse;
+
+    async fn from_request(req: Request, state: &S) -> Result<Option<Self>, Self::Rejection> {
+        <axum::Json<_> as axum::extract::OptionalFromRequest<S>>::from_request(req, state)
+            .await
+            .map_err(ErrorResponse::from)
+            .map(|value| value.map(|value| Json(value.0)))
     }
 }
